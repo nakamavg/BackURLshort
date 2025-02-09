c402f514b1205b8265bdf78869777fc6d9e965ba
Nakamavg <aecm.davidgomez@gmail.com>
2025-02-09 20:12:57 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+﻿# Fase de construcción
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /app
+
+# Copia el archivo del proyecto
+COPY *.csproj ./
+RUN dotnet restore
+
+# Copia el resto del código
+COPY . ./
+RUN dotnet publish -c Release -o out
+
+# Fase de ejecución
+FROM mcr.microsoft.com/dotnet/aspnet:9.0
+WORKDIR /app
+COPY --from=build /app/out .
+ENTRYPOINT ["dotnet", "UrlShortener.dll"]
--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls($"http://*:{Environment.GetEnvironmentVariable("PORT") ?? "5000"}");
 
 // Configurar la cadena de conexión desde la variable de entorno DATABASE_URL
-string databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+string databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL")
+                     ?? "postgresql://neondb_owner:npg_2GOZWE6NIeUR@ep-dry-field-a87aufuq-pooler.eastus2.azure.neon.tech/neondb?sslmode=require";
 
 if (string.IsNullOrEmpty(databaseUrl))
 {
